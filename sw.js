549f38df6a747e8d4486a5edb695e6684bcb63fd
Migue0301 <miguel.paredesperez1@gmail.com>
2025-10-17 13:47:31 -0600

    Commit 04

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Estructura bàsica de un Service Worker
 
 // !. Nombre del cachè y archivos a cachear 
-const CACHE_NAME = "Mi-cache-v2";
+const CACHE_NAME = "Mi-cache-v3";
 const urlsToCache = [
     "./",
     "index.html",
